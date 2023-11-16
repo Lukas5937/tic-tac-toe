82cca875fdc09bf90b4cabc6bfc817d72ae19496
Lukas5937 <lukas-reich@t-online.de>
2023-11-16 13:33:52 +0700

    Create factory functions and start playGame Object

--- js-file.js
+++ js-file.js
@@ -1,48 +1,81 @@
-const board = [];
-const rows = 3;
-const cols = 3;
+const boardFactory = (function() {
+        const boardArray = [];
+        const rows = 3;
+        const cols = 3;
+        const createBoard = function() {
+            for (let i = 0; i < rows; i++) {
+                boardArray[i] = [];
+                for (let j = 0; j < cols; j++) {
+                    boardArray[i].push("");
+                };
+            };};
+        createBoard();  
+    return {boardArray, cols, rows};
+})();
 
+const board = boardFactory.boardArray;
 
-// Create board method
-for (let i = 0; i < rows; i++) {
-    board[i] = ["hello"];
-    for (let j = 0; j < cols; j++) {
-        board[i].push("hello");
-    };
-};
-
-// this can be deleted later, it jus sets names for the cells for better understanding.
+/* this can be deleted later, it just sets names for the cells for better understanding.
 board[0] = ["hi", "os", "his"];
 board[1] = ["his", "his", "hi"];
 board[2] = ["hi", "hsi", "hi"];
 //board[0] = ["r1 c1", "r1 c2", "r1 c3"];
 //board[1] = ["r2 c1", "r2 c2", "r2 c3"];
-//board[2] = ["r3 c1", "r3 c2", "r3 c3"];
+//board[2] = ["r3 c1", "r3 c2", "r3 c3"]; */
 
 
-// here the Arrays for rows, columns and diagonals are created. They are needed in the checkWinner function.
-const rowArray = board;
-const colArray = [];
-const diag1 = [];
-const diag2 = [];
 
-for (i = 0; i < cols; i++) {
-    const column = board.map(row => row[i]);
-    colArray.push(column);
+//createPlayer factory Function
+const createPlayer = function(name, marker) {
+    return {name, marker}
+}
+
+player1 = createPlayer("Peter", "x");
+player2 = createPlayer("Marc", "o");
+
+// playGame Object / gameFlowFactory
+const gameFlowFactory = function() {
+    let isTurn = player1;
+    const playRound = function() {
+    board[0][0] = isTurn.marker;
+    isTurn === player1 ? isTurn = player2 : isTurn = player1;
+    };
+    return {playRound};
 };
+const game = gameFlowFactory();
+game.playRound();
+console.log(board);
+game.playRound();
+console.log(board);
 
-for (i = 0; i < rows; i++) {
-    const diagCell = board[i][i];
-    diag1.push(diagCell);
-};
 
-for (i = 0; i < rows; i++) {
-    const diagCell = board[i][(rows-1)-i];
-    diag2.push(diagCell);
-};
-const diagArray = [diag1, diag2];
 
+
+//the checkWinner function will check if the game is over.
 const checkWinner = function() {
+    // here the Arrays for rows, columns and diagonals are created.
+    const rowArray = board;
+    const colArray = [];
+    const diag1 = [];
+    const diag2 = [];
+
+    for (i = 0; i < boardFactory.cols; i++) {
+        const column = board.map(row => row[i]);
+        colArray.push(column);
+    };
+
+    for (i = 0; i < boardFactory.rows; i++) {
+        const diagCell = board[i][i];
+        diag1.push(diagCell);
+    };
+
+    for (i = 0; i < boardFactory.rows; i++) {
+        const diagCell = board[i][(boardFactory.rows-1)-i];
+        diag2.push(diagCell);
+    };
+    const diagArray = [diag1, diag2];
+
+    //now it will check if there is a winner
     gameOver = false;
     const checkIfEqual = function(array) {
         array.forEach(item => {
